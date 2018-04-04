4c70b3eb04dc9f62be86d5d1cfb6d6eec2f501e6
Marko Manninen <elonmedia@gmail.com>
2018-04-04 13:21:23 +0300

    added init file

---
+++ grcriddles/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+# file: __init__.py
+
+__version__ = "0.1.2"
+
+from .functions import Abnum, greek, syllabify, Counter, HTML, display_html, \
+                       display_side_by_side, remove, download_with_indicator, \
+                       unzip, joinpaths, get_file_size, copy_corpora, init_corpora, DataFrame, \
+                       process_greek_corpora, get_stats, get_database, search_words_from_corpora, \
+                       all_greek_text_file, perseus_greek_text_file, first1k_greek_text_file, \
+                       csv_file_name, perseus_zip_file, perseus_zip_dir, perseus_tmp_dir, \
+                       perseus_dir, first1k_zip_file, first1k_zip_dir, first1k_tmp_dir, \
+                       first1k_dir
